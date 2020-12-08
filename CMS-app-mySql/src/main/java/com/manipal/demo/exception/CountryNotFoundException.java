90232be1005f8a94152c02b3489d5cc935565d75
aman <anshujai4@gmail.com>
2020-12-08 22:39:18 +0530

    cms-app

---
+++ CMS-app-mySql/src/main/java/com/manipal/demo/exception/CountryNotFoundException.java
@@ -0,0 +1,15 @@
+package com.manipal.demo.exception;
+
+public class CountryNotFoundException extends RuntimeException {
+
+public CountryNotFoundException() {}
+    
+
+    public CountryNotFoundException( String message) {
+        super(message);
+        
+    }
+	
+	
+
+}
